f5ea30ac5d1460f4d9af715d4abb9c10909e546c
iamspal <sumitpal.spal@gmail.com>
2023-11-01 21:08:48 +0100

    Add api calls functions

---
+++ src/services/gifs.js
@@ -0,0 +1,26 @@
+export async function getGifs() {
+  try {
+    let result = await fetch(import.meta.env.VITE_IMAGE_CLICKER_API_URL + 'images')
+    result = await result.json()
+    return result.data
+  } catch {
+    console.error('Error fetching gifs')
+    return []
+  }
+}
+
+export async function upVoteGif(id) {
+  try {
+    const form = new FormData()
+    form.append('id', id)
+    let result = await fetch(import.meta.env.VITE_IMAGE_CLICKER_API_URL + 'vote', {
+      method: 'POST',
+      body: form
+    })
+    result = await result.json()
+    return result.success
+  } catch {
+    console.error('Error up vote gif')
+    return false
+  }
+}

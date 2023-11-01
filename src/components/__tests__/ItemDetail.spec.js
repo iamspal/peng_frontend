2acf90a1541bc2c3ac7b2962d10a172cd52d49db
iamspal <sumitpal.spal@gmail.com>
2023-11-02 00:03:35 +0100

    Lint

--- src/components/__tests__/ItemDetail.spec.js
+++ src/components/__tests__/ItemDetail.spec.js
@@ -1,54 +1,50 @@
 import { describe, it, expect } from 'vitest'
 import { mount } from '@vue/test-utils'
-import ItemListDetail from '@/components/ItemDetail.vue';
+import ItemListDetail from '@/components/ItemDetail.vue'
 
 describe('ItemListDetail', () => {
-    it('renders item details and emits image-clicked event on image click', async () => {
-        // Create a mock item
-        const item = {
-            id: 1,
-            title: 'Test Item',
-            url: 'test-url.jpg',
-            votes: 5,
-        };
+  it('renders item details and emits image-clicked event on image click', async () => {
+    // Create a mock item
+    const item = {
+      id: 1,
+      title: 'Test Item',
+      url: 'test-url.jpg',
+      votes: 5
+    }
 
-        // Mount the component with the mock item
-        const wrapper = mount(ItemListDetail, {
-            props: {
-                item: item,
-            },
-        });
+    // Mount the component with the mock item
+    const wrapper = mount(ItemListDetail, {
+      props: {
+        item: item
+      }
+    })
 
-        // Find elements within the component
-        const titleElement = wrapper.find('h2');
-        const imgElement = wrapper.find('img');
-        const votesElement = wrapper.find('p');
+    // Find elements within the component
+    const titleElement = wrapper.find('h2')
+    const imgElement = wrapper.find('img')
+    const votesElement = wrapper.find('p')
 
-        // Expect item details to be rendered
-        expect(titleElement.text()).toBe(item.title);
-        expect(imgElement.attributes('src')).toBe(item.url);
-        expect(votesElement.text()).toContain(`Votes: ${item.votes}`);
+    // Expect item details to be rendered
+    expect(titleElement.text()).toBe(item.title)
+    expect(imgElement.attributes('src')).toBe(item.url)
+    expect(votesElement.text()).toContain(`Votes: ${item.votes}`)
 
-        // Click the image
-        await imgElement.trigger('click');
+    // Click the image
+    await imgElement.trigger('click')
+    // Expect the emitted event with the item's id
+    expect(wrapper.emitted('image-clicked')).toBeTruthy()
+    expect(wrapper.emitted('image-clicked')[0]).toEqual([item.id])
+  })
 
-        // Wait for the next tick of the event loop to ensure the event is emitted
-        await wrapper.vm.$nextTick();
+  it('does not render anything if item.title is undefined', () => {
+    // Mount the component with item.title undefined
+    const wrapper = mount(ItemListDetail, {
+      props: {
+        item: { id: 1 }
+      }
+    })
 
-        // Expect the emitted event with the item's id
-        expect(wrapper.emitted('image-clicked')).toBeTruthy();
-        expect(wrapper.emitted('image-clicked')[0]).toEqual([item.id]);
-    });
-
-    it('does not render anything if item.title is undefined', () => {
-        // Mount the component with item.title undefined
-        const wrapper = mount(ItemListDetail, {
-            props: {
-                item: { id: 1 },
-            },
-        });
-
-        // Expect the component to not render anything
-        expect(wrapper.html()).toBe('<!--v-if-->');
-    });
-});
+    // Expect the component to not render anything
+    expect(wrapper.html()).toBe('<!--v-if-->')
+  })
+})
--- src/components/__tests__/ItemList.spec.js
+++ src/components/__tests__/ItemList.spec.js
@@ -1,31 +1,27 @@
 import { describe, it, expect } from 'vitest'
 import { mount } from '@vue/test-utils'
-import ItemList from '@/components/ItemList.vue';
+import ItemList from '@/components/ItemList.vue'
 
 describe('ItemList', () => {
-    it('renders items and emits item-clicked event on click', async () => {
-        // Create a mock item
-        const item = { id: 1, title: 'Test Item' };
+  it('renders items and emits item-clicked event on click', async () => {
+    // Create a mock item
+    const item = { id: 1, title: 'Test Item' }
 
-        // Mount the component with the mock item
-        const wrapper = mount(ItemList, {
-            props: {
-                items: [item],
-            },
-        });
+    // Mount the component with the mock item
+    const wrapper = mount(ItemList, {
+      props: {
+        items: [item]
+      }
+    })
 
-        // Find the list item element and click it
-        const listItem = wrapper.find('li');
-        listItem.trigger('click');
+    // Find the list item element and click it
+    const listItem = wrapper.find('li')
+    listItem.trigger('click')
+    // Expect the emitted event
+    expect(wrapper.emitted('item-clicked')).toBeTruthy()
+    expect(wrapper.emitted('item-clicked')[0]).toEqual([item])
 
-        // Wait for the next tick of the event loop to ensure the event is emitted
-        await wrapper.vm.$nextTick();
-
-        // Expect the emitted event
-        expect(wrapper.emitted('item-clicked')).toBeTruthy();
-        expect(wrapper.emitted('item-clicked')[0]).toEqual([item]);
-
-        // Expect the item to be rendered
-        expect(listItem.text()).toBe(item.title);
-    });
-});
+    // Expect the item to be rendered
+    expect(listItem.text()).toBe(item.title)
+  })
+})
